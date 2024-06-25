cd00f2cf2051b354b7e64e5f8785637bc3a3dc84
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-06-25 09:27:05 -0700

    updated examples

--- examples/example_script.py
+++ examples/example_script.py
@@ -8,14 +8,12 @@ import os
 import shutil
 import time
 from dotenv import load_dotenv
-from llm_eval.eval import EvalResult, eval_result_summarizer
+from llm_eval.eval import EvalResult
 from llm_eval.eval import EvalHarness
-from pprint import pprint
 
 load_dotenv()
 
 DIR_PATH = "__temp__"
-
 
 
 def print_result(result: EvalResult) -> None:
@@ -29,8 +27,6 @@ def print_result(result: EvalResult) -> None:
 
 def main() -> None:
     """Run the main function."""
-    # wandb.init(project="test-project")
-
     if os.path.exists(DIR_PATH):
         shutil.rmtree(DIR_PATH)
     os.makedirs(DIR_PATH)
@@ -41,23 +37,19 @@ def main() -> None:
         # async_batch_size=1,
         callback=print_result,
     )
-    # eval_harness.add_eval_from_yamls('../evals/evals')
-    # eval_harness.add_candidate_from_yamls('../evals/candidates')
-
-    eval_harness.add_eval_from_yaml('../examples/evals/mask_emails.yaml')
-    eval_harness.add_eval_from_yaml('../examples/evals/mask_emails.yaml')
-    eval_harness.add_candidate_from_yaml('../examples/candidates/openai_3.5_1106_multiple_parameters.yaml')
-    # eval_harness.add_candidate_from_yaml('../examples/candidates/openai_3.5_1106.yaml')
-    # eval_harness.add_candidate_from_yaml('../examples/candidates/openai_3.5_1106.yaml')
-    # eval_harness.add_candidate_from_yaml('../examples/candidates/openai_4.0_1106.yaml')
+    # example of adding all evals in a directory
+    eval_harness.add_evals_from_yamls('../examples/evals/*.yaml')
+    # example of adding candidates one at a time
+    eval_harness.add_candidate_from_yaml('../examples/candidates/openai_3.5.yaml')
+    eval_harness.add_candidate_from_yaml('../examples/candidates/openai_4.0.yaml')
 
     print('start')
     start = time.time()
     results = eval_harness()
     end = time.time()
-    # for r in results:
-    #     for a in r:
-    #         print(a)
+    for r in results:
+        for a in r:
+            print(a)
     print(f"Total time: {end - start}")
 
 
