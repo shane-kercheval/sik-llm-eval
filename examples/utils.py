bdf37b342a90696a69f3d29d03508ad262b14edc
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-08-19 15:05:16 -0700

    Updated first example notebook

---
+++ examples/utils.py
@@ -0,0 +1,14 @@
+from llm_eval.eval import EvalResult
+
+
+def print_result(result: EvalResult) -> None:
+    """
+    This function is used as a callback and prints the results of each evaluation.
+
+    The callback can also be used, for example, to save the results to a file. If you're
+    running a large number of evaluations, you may want to save the results to a file
+    periodically in case there are issues/errors before the entire EvalHarness completes.
+    """  # noqa: D404
+    print(f"Num Checks: {result.num_checks}")
+    print(f"Num Successful Checks: {result.num_successful_checks}")
+    print('---')
